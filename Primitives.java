bfb2e1c0f318d4739b6b310fc57aed018ab21621
purcellconsult <purcellconsult@gmail.com>
2016-10-13 13:50:24 -0700

    simple java program that shows the 8-primitives

---
+++ Primitives.java
@@ -0,0 +1,32 @@
+/********************************
+*
+* This is a simple class that
+* introduces the 8 primitive types in Java.
+* Java is a statically-typed language, meaning
+* primitives must be declared before they can be 
+* used.  
+*
+* @author: Doug Purcell - Purcell Consult 
+* website: http://www.purcellconsult.com
+*
+* Email: admin@purcellconsult.com for 
+* onsite Java or agile training 
+*
+**************************************/
+
+	public class Primitives {
+		
+		public static void main(String [] args) {
+			
+			boolean vacationDay = true;
+			byte number = 127;
+			char c = 'C';
+			short s = 256;
+			int i = 10;
+			long l = 74207281;
+			float f = 3.57f;
+			double d = 3.14159;
+			
+		}
+		
+	}
